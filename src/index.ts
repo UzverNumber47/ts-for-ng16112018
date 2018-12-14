6e83d59f491d9212248421cf0531400127cbacb9
Dmitry Borisevich <dmborisevich@gmail.com>
2018-12-14 18:21:07 +0300

    feat(index): search github repositories

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,53 @@
-import { from } from 'rxjs';
-import { map, observeOn } from 'rxjs/operators';
-import { asap } from 'rxjs/internal/scheduler/asap';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map, switchMap, distinctUntilChanged, pluck } from 'rxjs/operators';
 
-console.log('hi all');
+type GithubRepo = {name: string, html_url: string};
+
+const input: HTMLInputElement = document.getElementById('input') as HTMLInputElement;
+const output: HTMLDivElement = document.getElementById('output') as HTMLDivElement;
+
+const inputObservable$ = fromEvent(input, 'keyup').pipe(
+    debounceTime(300),
+    pluck('target', 'value'),
+    distinctUntilChanged(),
+    switchMap(requestRepositories, (_searchStr: any, res: any) => res.items),    
+    map((result: GithubRepo[]) => {
+        clearOutput(output);
+        if (!result) {
+            output.appendChild(buildEmptyResult());
+        } else {
+            result.forEach((repo: GithubRepo) => {
+                const row: HTMLElement = buildResultRow(repo);
+                output.appendChild(row);
+            });
+        }
+        return result;
+    })
+);
+
+inputObservable$.subscribe((items) => {
+    console.log('Request finished', items);    
+});
+
+function requestRepositories(searchString: any): any {
+    console.log('Search string', searchString);
+    return fetch(`https://api.github.com/search/repositories?q=${searchString}`).then(res => res.json());
+}
+
+function buildResultRow(repo: GithubRepo): HTMLElement {
+    const element: HTMLElement = document.createElement('DIV');
+    element.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>`;
+    return element;
+}
+
+function buildEmptyResult(): HTMLElement {
+    const element: HTMLElement = document.createElement('DIV');
+    return element;
+}
+
+function clearOutput(output: HTMLDivElement): void {
+    output.innerHTML = '';
+}
 
 // const sequence1$: Observable<number> = interval(1000).pipe(take(5));
 //
@@ -14,13 +59,13 @@ console.log('hi all');
 //     console.log(v);
 // });
 // const clickObservable$ = fromEvent(document, 'click');
-//
+
 // // tslint:disable-next-line
 // function performRequest(): any {
 //     return fetch('https://jsonplaceholder.typicode.com/users/1')
 //         .then(((res: Response) => res.json()));
 // }
-//
+
 // clickObservable$
 //     .pipe(
 //         // tslint:disable-next-line
@@ -72,18 +117,18 @@ console.log('hi all');
 //         console.log(`Sub 2 =>>>`, value);
 //     });
 // }, 5000);
-const arr: number [] = [];
-for (let i: number = 0; i < 10000; i++) {
-    arr.push(i);
-}
-console.log('Start');
-console.time('Schedule');
-from(arr).pipe(
-    observeOn(asap),
-    map((v: number) => v * 2 % 3)
-).subscribe(() => {
-}, () => {
-}, () => {
-    console.timeEnd('Schedule');
-});
-console.log('End');
+// const arr: number [] = [];
+// for (let i: number = 0; i < 10000; i++) {
+//     arr.push(i);
+// }
+// console.log('Start');
+// console.time('Schedule');
+// from(arr).pipe(
+//     observeOn(asap),
+//     map((v: number) => v * 2 % 3)
+// ).subscribe(() => {
+// }, () => {
+// }, () => {
+//     console.timeEnd('Schedule');
+// });
+// console.log('End');
